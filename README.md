de2b64e4db26670ef67d5649c3acf95d386e2065
nicolas <nicolas.braga@tarkuss.com.br>
2022-11-28 11:13:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Bibliotecas
+
+yarn add axios
+
+yarn add @chakra-ui/react @emotion/react @emotion/styled framer-motion
+
+Para desenvolvimento
+yarn add -D react-icons react-hook-form yup react-dropzone react-input-mask react-cookie moment
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </>
+  );
+};
+
+export default Home;
